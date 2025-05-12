7ade0f0e6eb48786c9bf91a050c6a970f6287f8b
Arfazappu <Arfazappu@users.noreply.github.com>
2025-05-12 08:06:20 +0000

    deploy: 9e30312e527c646795a53533c0114d4fe68d138b

---
+++ assets/__federation_shared_react-IF3jPPLj.js
@@ -0,0 +1,6 @@
+import { r as requireReact, a as getDefaultExportFromCjs } from './index-jL0mSeQk.js';
+
+var reactExports = requireReact();
+const index = /*@__PURE__*/getDefaultExportFromCjs(reactExports);
+
+export { index as default };
---
+++ assets/__federation_shared_react-dom-qJoYPOCO.js
@@ -0,0 +1,7 @@
+import { a as getDefaultExportFromCjs } from './index-jL0mSeQk.js';
+import { r as requireReactDom } from './index-BVuIqp-V.js';
+
+var reactDomExports = requireReactDom();
+const index = /*@__PURE__*/getDefaultExportFromCjs(reactDomExports);
+
+export { index as default };
