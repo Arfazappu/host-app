4ed972d9c17bd1997c70f567a1ecfcb67ea4784c
Arfaz Ahmed <108565133+Arfazappu@users.noreply.github.com>
2025-05-13 14:08:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/host-app/',
   plugins: [react(),
   federation({
     name: 'hostApp',
