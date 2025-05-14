d8cb9c4d8ba0187dcb3399b80d8b027bd782b1a5
Arfaz Ahmed <108565133+Arfazappu@users.noreply.github.com>
2025-05-14 12:03:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,14 @@ import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/host-app/',
   plugins: [react(),
   federation({
     name: 'hostApp',
     remotes: {
       // loginApp: 'http://localhost:4173/assets/remoteEntry.js',
       // taskApp: 'http://localhost:4174/assets/remoteEntry.js',
-      loginApp: 'https://arfazappu.github.io/login-remote-app/assets/remoteEntry.js',
-      taskApp: 'https://arfazappu.github.io/task-remote-app/assets/remoteEntry.js',
+      loginApp: 'https://login-remote-app.vercel.app/assets/remoteEntry.js',
+      taskApp: 'https://task-remote-app.vercel.app/assets/remoteEntry.js',
     },
     shared: ['react', 'react-dom', 'react-router-dom'],
   })
